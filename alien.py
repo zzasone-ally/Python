8f00ddf7ed2f96dbc61c80181ee0476c4b9fd2e5
zzasone-ally <1723272490@qq.com>
2022-02-17 11:05:32 +0800

    字典练习

--- alien.py
+++ alien.py
@@ -1,3 +1,11 @@
-alien_0={'color':'green','points':5}
-print(alien_0['color'])
-print(alien_0['points'])
+alien={'x':0,'y':25,'speed':'medium'}
+alien['speed']= 'fast'
+print("original x: " +str(alien['x']))
+if alien['speed'] =='slow':
+	x_increment =1
+elif alien['speed'] == 'medium':
+	x_increment =2
+else:
+	x_increment =3
+alien['x']=alien['x']+x_increment
+print("new_x: "+str(alien['x']))
---
+++ alien2.py
@@ -0,0 +1,5 @@
+alien={'color':'green','point':5}
+print(alien)
+
+del alien['point']
+print (alien)
